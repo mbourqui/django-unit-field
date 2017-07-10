947c441b84c8b264db0defb0f5b99f67c58389e2
Marc Bourqui <marc@bourqui.org>
2017-07-10 22:03:20 +0200

    Update project metadata

--- setup.py
+++ setup.py
@@ -1,18 +1,53 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from setuptools import find_packages
+
+import os
+
+from setuptools import setup, find_packages
+
+from unitfield import __version__
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-unitfield',
-    version='0.0.2',
-    author=u'Christian Kohout',
-    author_email='ck@getaweb.at',
-    packages=find_packages(),
+    version=__version__,
+    author=u'Marc Bourqui',
+    author_email='pypi.kemar@bourqui.org',
+    license='UNLICENSE',
+    description='Django specialized model field inherited from FloatField with unit conversion by custom widget',
+    long_description=README,
     url='https://github.com/mbourqui/django-unitfield',
-    license='UNLICENSE, see UNLICENSE.txt',
-    description='a new field inherited from FloatField with a custom widget' + \
-                ' that allows unit conversion',
-    long_description=open('README.rst').read(),
+    packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'Django>=1.9.13',
+    ],
     zip_safe=False,
+    keywords='django',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Utilities',
+    ],
 )
--- unitfield/__init__.py
+++ unitfield/__init__.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+__license__ = 'Unlicense <http://unlicense.org/>'
+__authors__ = ['Christian Kohout <ck@getaweb.at>', 'Marc Bourqui <pypi.kemar@bourqui.org>']
+__docformat__ = 'numpy'
+__version__ = '0.0.0'
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+__status__ = 'Stable'
